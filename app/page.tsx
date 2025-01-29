fc8d40ee836b6dcd559d3a7c2c269c158175fa6f
2732ii2 <mo.anas123khan@gmail.com>
2025-01-29 18:43:13 +0530

    initializing the project

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Header/>
+    </div>
+  );
+}
+
+const Header=()=>{
+  return <div className={`Header flex  `}>
+    <h1>User DB</h1>
+
+    <div className={`  `}></div>
+     </div>
+}
